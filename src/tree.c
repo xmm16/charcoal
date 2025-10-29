b9780eed8c64d668b5e25daaef2e5f07cfa2e0b7
xmm16 <eshaan2031@icloud.com>
2025-10-29 10:47:24 -0500

    Update tree.c

--- src/tree.c
+++ src/tree.c
@@ -1,37 +1,20 @@
 #include "tree.h"
 #include <stdlib.h>
 
-p_node create_tree(p_node_t type, p_node_r rank){
+p_node create_tree(p_node_t type, p_node_r rank, size_t start, size_t end){
   p_node result;
   result.type = type;
   result.rank = rank;
-  result.next = malloc(sizeof(p_node*));
+  result.left = malloc(sizeof(p_node*));
+  result.right = malloc(sizeof(p_node*));
   result.back = NULL; 
   return result;
 }
 
-void insert_tree(p_node* tree, size_t index, p_node_t type, p_node_r rank){
-  p_node* tree_at_index = tree;
-  p_node* previous_tree;
-
-  for (int i = 0; i < index; i++){
-    tree_at_index = tree_at_index->next;
-    if (i == index - 2) previous_tree = tree_at_index;
-  }
-
-  tree_at_index->type = type;
-  tree_at_index->rank = rank;
-  tree_at_index->next = malloc(sizeof(p_node*));
-  tree_at_index->back = previous_tree;
+void insert_tree(p_node* tree, p_node* back, p_node_t type, p_node_r rank, size_t start, size_t end){
+  tree->type = type;
+  tree->rank = rank;
+  tree->left = malloc(sizeof(p_node*));
+  tree->right = malloc(sizeof(p_node*));
+  tree->back = back;
 }
-
-p_node* access_tree(p_node* tree, size_t index){
-  p_node* tree_at_index = tree;
-
-  for (int i = 0; i < index; i++){
-    tree_at_index = tree_at_index->next;
-  }
-
-  return tree_at_index;
-}
-
