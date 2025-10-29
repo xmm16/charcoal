54f652e72ff94b1e746b3bd4f211f3daf6df3b3a
xmm16 <eshaan2031@icloud.com>
2025-10-29 10:48:44 -0500

    Add start and end properties to tree nodes

--- src/tree.c
+++ src/tree.c
@@ -5,6 +5,8 @@ p_node create_tree(p_node_t type, p_node_r rank, size_t start, size_t end){
   p_node result;
   result.type = type;
   result.rank = rank;
+  result.start = start;
+  result.end = end;
   result.left = malloc(sizeof(p_node*));
   result.right = malloc(sizeof(p_node*));
   result.back = NULL; 
@@ -14,6 +16,8 @@ p_node create_tree(p_node_t type, p_node_r rank, size_t start, size_t end){
 void insert_tree(p_node* tree, p_node* back, p_node_t type, p_node_r rank, size_t start, size_t end){
   tree->type = type;
   tree->rank = rank;
+  tree->start = start;
+  tree->end = end;
   tree->left = malloc(sizeof(p_node*));
   tree->right = malloc(sizeof(p_node*));
   tree->back = back;
