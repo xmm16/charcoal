74a8e52d53f6743c8528a56023af92c367c4aef6
eshnd <eshaan2031@icloud.com>
2025-10-28 23:38:25 -0500

    format changes

--- src/main.c
+++ src/main.c
@@ -4,17 +4,25 @@
 int main(int argc, char** argv){
   size_t code_lex_index;
   token_ll lexed_code = lex(argv[1], &code_lex_index);
-  printf("code_lex_index = %zu\n", code_lex_index);
-
   for (int i = 0; i < code_lex_index; i++){
     int id = token_from_ll(lexed_code, i)->id;
     switch (id){
-
       case 128:
-        printf("id: \narg: %s\n", token_from_ll(lexed_code, i)->args);
+        printf("id: WORD\narg: %s\n", token_from_ll(lexed_code, i)->args);
+        break;
+
+      case 129:
+        printf("id: INT\narg: %s\n", token_from_ll(lexed_code, i)->args);
+        break;
+
+      case 130:
+        printf("id: FLOAT\narg: %s\n", token_from_ll(lexed_code, i)->args);
+        break;
+
       default:
-        printf("id: %c\narg: %s\n", id, token_from_ll(lexed_code, i)->args);
+        printf("id: (symbol) %c\narg: %s\n", id, token_from_ll(lexed_code, i)->args);
     }
-    
+
+    printf("\n");
   }
 }
